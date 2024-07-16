7cc88fb7248c079141bb5d26b93c84a9bf6e4f3f
JirapongChanthaleka <s6506022620061@email.kmutnb.ac.th>
2024-07-16 12:04:24 +0700

    update

---
+++ L01/example_non_blocking2.py
@@ -0,0 +1,8 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.50ck_STREAM)
+sock.connect(('localhost', 1234))
+sock.setblocking(0)
+
+data = 'foobar\n' * 10 * 1024 * 1024 # 70 MB of data
+sent = sock.send(data.encode())
+assert sent == len(data), '%s != %s' % (sent, len(data))
---
+++ L02/smtp-send.py
@@ -0,0 +1,4 @@
+import smtplib
+
+message = """From: From Joe <joe@blow.com>
+To: To Henry"""
