6f908f13813890d1b7db1a6b3a67000fccc1f368
JirapongChanthaleka <s6506022620061@email.kmutnb.ac.th>
2024-07-23 11:56:28 +0700

    update L03

---
+++ L03/telnet.py
@@ -0,0 +1,19 @@
+import getpass
+import telnetlib
+
+HoST = "192.168.1.124"
+user = input("Enter your remote accountL ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"login: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
